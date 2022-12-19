d8f2ced30dcb9050551ef8276c48929d97a6745e
Drikus Roor <drikusroor@gmail.com>
2022-12-19 20:22:15 +0100

    Add script that converts organization addresses from json to csv file

---
+++ addresses.js
@@ -0,0 +1,39 @@
+import fs from "fs";
+
+const organizations = JSON.parse(fs.readFileSync("organizations.json"));
+
+const fields = [
+  "tblPointOfInterestName",
+  "tblPointOfInterestStreet",
+  "tblPointOfInterestNumber",
+  "tblPointOfInterestNumberAddition",
+  "tblPointOfInterestPostalCode",
+  "tblPointOfInterestCity",
+  "country",
+  "tblPointOfInterestTelephone",
+  "tblPointOfInterestWebsite",
+  "tblPointOfInterestEmail",
+];
+
+const addresses = organizations.map((item) => {
+  const { details } = item;
+  const address = fields.map((field) => details[field]).join(", ");
+  return address;
+});
+
+const headers = [
+  "organisatie",
+  "straat",
+  "huisnummer",
+  "toevoeging",
+  "postcode",
+  "plaats",
+  "land",
+  "telefoon",
+  "website",
+  "email",
+].join(",");
+
+addresses.unshift(headers);
+
+fs.writeFileSync("addresses.csv", addresses.join("\n"));
